1c4467d6c89d952603d2052b0053ad8db56c1188
Miguel Calvache <calvache117@gmail.com>
2023-11-24 11:33:58 +0100

    The core code was included.
    I did it because I had no code before.

--- count_lines.py
+++ count_lines.py
@@ -1,1 +1,12 @@
+import sys
+"""
+This script counts the lines in standard input 
+Input: text from the system
+"""
 print("hello you")
+
+count = 0
+for line in sys.stdin:
+	count +=1
+
+print(count, 'lines in standard input')
